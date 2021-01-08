1984395871226393bb71d40108d2c19a7d1b49a3
Micael Sousa <micaelsousabrb@gmail.com>
2021-01-07 21:26:36 -0300

    GetHashCode e Equals

---
+++ GetHashCode and Equals/GetHashCode and Equals/Entities/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetHashCode_and_Equals.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        // Verifica se é igual apenas pelo e-mail
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Client))
+                return false;
+
+            Client other = obj as Client;
+
+            return Email.Equals(other.Email);
+        }
+
+        // Verifica se é igual apenas pelo e-mail
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ GetHashCode and Equals/GetHashCode and Equals/Program.cs
@@ -0,0 +1,50 @@
+﻿using GetHashCode_and_Equals.Entities;
+using System;
+
+namespace GetHashCode_and_Equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GetHashCode e Equals
+
+            // São operadores da classe Object utilizadas para comparar se um objeto é igual a outro
+
+            // Equals: Lento, 100% de resposta
+            // GetHashCode: Rápido, porém resposta não é 100%
+
+            // Os tipos pré-definidos já possuem implementação para essas operações. 
+            // Classes e structs personalizados precisam sobrepô-las.
+
+            // Equals é o método que compara se o objeto é igual a outro, retornando true ou false
+            string a = "Maria";
+            string b = "Alex";
+
+            Console.WriteLine(a.Equals(b));
+
+            string c = "Mike";
+            string d = "Mike";
+
+            Console.WriteLine(c.Equals(d));
+
+            // GetHashCode é o método que retorna um número inteiro representando um código gerado a partir das informações do objeto
+            Console.WriteLine("\n\n" + a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            Console.WriteLine(c.GetHashCode());
+            Console.WriteLine(d.GetHashCode());
+
+            // Regra de ouro do GetHashCode: 
+            // Se o código de dois objetos for diferente, então os dois objetos são diferentes
+            // Se o código de dois objetos for igual, MUITO PROVAVELMENTE os objetos são iguais (pode haver colisão)
+
+            Client ca = new Client { Name = "Maria", Email = "maria@gmail.com"  };
+            Client cb = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine("\n\n" + ca.Equals(cb));
+            Console.WriteLine(ca.GetHashCode());
+            Console.WriteLine(cb.GetHashCode());
+        }
+    }
+}
